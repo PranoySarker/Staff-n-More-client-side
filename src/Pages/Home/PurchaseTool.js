737d44a495e5d2d1bb588cf706363c520ee2b3c3
PranoySarker <pranoysarker01@gmail.com>
2022-05-26 18:45:39 +0600

    purchase form

--- src/Pages/Home/PurchaseTool.js
+++ src/Pages/Home/PurchaseTool.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
 import auth from '../../firebase.init';
 
@@ -7,6 +8,8 @@ const PurchaseTool = () => {
     const { id } = useParams()
     const [tool, setTool] = useState({});
     const [user] = useAuthState(auth);
+    const { register, formState: { errors }, handleSubmit } = useForm({ mode: "onChange" });
+
 
     useEffect(() => {
         const url = `http://localhost:5000/tools/${id}`;
@@ -14,6 +17,9 @@ const PurchaseTool = () => {
             .then(res => res.json())
             .then(data => setTool(data))
     }, [])
+
+
+    const onSubmit = data => console.log(data);
 
     return (
         <div className='my-10'>
@@ -32,21 +38,111 @@ const PurchaseTool = () => {
                 </div>
 
                 <div class="card w-full bg-base-300 shadow-xl mb-5">
-                    <div class="card-body">
-                        <form action="" className='mx-auto w-1/2'>
-                            <input type="text" value={user?.displayName} class="input input-bordered input-info w-full max-w-xs text-xl mb-3" />
+                    <div class="card-body mx-auto w-3/5">
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <div className="form-control w-full max-w-xs mb-3">
 
-                            <input type="text" value={user.email} disabled class="input input-bordered input-info w-full max-w-xs text-xl mb-3" />
+                                <input type="text"
+                                    placeholder="Name"
+                                    value={user?.displayName}
+                                    name='name'
+                                    disabled
+                                    className="input input-bordered w-full max-w-xs bg-base-100"
+                                    readOnly />
+                            </div>
 
-                            <input type="text" placeholder="Your Phone Number " class="input input-bordered input-info w-full max-w-xs text-xl mb-3" />
+                            <div className="form-control w-full max-w-xs mb-3">
 
-                            <input type="text" placeholder="Your Address" class="input input-bordered input-info w-full max-w-xs text-xl mb-3" />
+                                <input type="email"
+                                    placeholder="Email"
+                                    value={user?.email}
+                                    name='email'
+                                    disabled
+                                    className="input input-bordered w-full max-w-xs bg-base-100"
+                                />
+                            </div>
 
-                            <label className='label'>
-                                <span class="label-text">Place Your Order Here</span>
-                            </label>
-                            <input type="number" class="input input-bordered input-info w-full max-w-xs text-xl mb-3" />
+                            <div className="form-control w-full max-w-xs">
+
+                                <input type="text"
+                                    placeholder="Phone Number"
+                                    name='phone'
+                                    className="input input-bordered w-full max-w-xs bg-base-100"
+                                    {...register("phone", {
+                                        required: {
+                                            value: true,
+                                            message: 'phone is Required'
+                                        },
+                                    })}
+                                />
+                                <label className="label">
+                                    {errors.phone?.type === 'required' && <span className='label-text-alt text-red-500'>{errors.phone.message}</span>}
+                                </label>
+
+                            </div>
+
+                            <div className="form-control w-full max-w-xs">
+
+                                <input type="text"
+                                    placeholder="Address"
+                                    name='address'
+                                    className="input input-bordered w-full max-w-xs bg-base-100"
+                                    {...register("address", {
+                                        required: {
+                                            value: true,
+                                            message: 'Address is Required'
+                                        },
+                                    })}
+                                />
+                                <label className="label">
+                                    {errors.address?.type === 'required' && <span className='label-text-alt text-red-500'>{errors.address.message}</span>}
+
+                                </label>
+
+                            </div>
+                            <div className="form-control w-full max-w-xs mb-3">
+                                <input type="text"
+                                    value={tool.name}
+                                    name='toolName'
+                                    disabled
+                                    className="input input-bordered w-full max-w-xs bg-base-100"
+                                />
+                            </div>
+
+                            <div className="form-control w-full max-w-xs ">
+
+                                <input type="text"
+                                    placeholder="Order Quantity"
+                                    name='order'
+                                    // value={tool.minimumOrder}
+                                    className="input input-bordered w-full max-w-xs bg-base-100"
+                                    {...register("order", {
+                                        required: {
+                                            value: true,
+                                            message: 'please fill order'
+                                        },
+                                        min: {
+                                            value: `${tool.minimumOrder}`,
+                                            message: 'Order must be more than Minimum order quantity'
+                                        },
+                                        max: {
+                                            value: `${tool.availableQuantity}`,
+                                            message: 'Order must not more than Available quantity'
+                                        }
+                                    })}
+                                />
+                                <label className="label">
+                                    {errors.order?.type === 'required' && <span className='label-text-alt text-red-500'>{errors.order.message}</span>}
+                                    {errors.order?.type === 'min' && <span className='label-text-alt text-red-500'>{errors.order.message}</span>}
+                                    {errors.order?.type === 'max' && <span className='label-text-alt text-red-500'>{errors.order.message}</span>}
+                                </label>
+
+                            </div>
+                            <input className='btn w-full max-w-xs text-lg' type="submit" value='Place Order' disabled={errors.order?.type === 'min' || errors.order?.type === 'max'} />
+
+
                         </form>
+
                     </div>
                 </div>
             </div>
